7daff757cdd35d7e69eab13921600554dfa5b6ae
Suryakant pandey <suryapandey0211@gmail.com>
2021-06-26 22:19:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var p1 = Math.floor(Math.random()*6+1);
+var p2 = Math.floor(Math.random()*6+1);
+
+console.log(p1);
+console.log(p2);
+document.querySelector(".img1").src="images/dice"+p1+".png";
+document.querySelector(".img2").src="images/dice"+p2+".png";
+
+if(p1>p2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(p2>p1)
+{
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw";
+}
