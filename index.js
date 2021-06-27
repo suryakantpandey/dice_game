4d77af5797bef15073b811859933b0c9e1a50728
Suryakant pandey <suryapandey0211@gmail.com>
2021-06-27 10:33:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+  function refresh()
+  {
 
-var p1 = Math.floor(Math.random()*6+1);
-var p2 = Math.floor(Math.random()*6+1);
+  var p1 = Math.floor(Math.random()*6+1);
+  var p2 = Math.floor(Math.random()*6+1);
+  document.querySelector(".img1").src="images/dice"+p1+".png";
+  document.querySelector(".img2").src="images/dice"+p2+".png";
 
-console.log(p1);
-console.log(p2);
-document.querySelector(".img1").src="images/dice"+p1+".png";
-document.querySelector(".img2").src="images/dice"+p2+".png";
-
-if(p1>p2)
-{
-  document.querySelector("h1").innerHTML = "Player 1 Wins";
-}
-else if(p2>p1)
-{
-  document.querySelector("h1").innerHTML = "Player 2 Wins";
-}
-else
-{
-  document.querySelector("h1").innerHTML = "Draw";
-}
+  if(p1>p2)
+  {
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+  }
+  else if(p2>p1)
+  {
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+  }
+  else
+  {
+    document.querySelector("h1").innerHTML = "Draw";
+  }
+  }
